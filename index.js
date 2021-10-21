c17166e80652671ee23194433a97129c4b2a3e92
Cortney Logan <logan.cort@gmail.com>
2021-10-21 08:44:17 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// pressing play starts the game
+// ---------- disables start button once clicked
+// ---------- status displays counts up to 3 ("Starting in 1...2...3..Go!")
+
+// Once the counter counts up to 3
+// ---------- the user buttons are enabled and user can select an option (RPS)
+// ---------- the game records what option is selected
+// ---------- the computer randomly picks an option (RPS)
+
+// Once the user picks an option
+// ---------- compares user and comp choices and decides who wins
+// ---------- displays who wins and updated score
+// ---------- play button is enabled and game is reset
